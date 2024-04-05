cd5f35600a7fb382e983cacedf46b66bf81a1cc2
damonryon <damon.ryon@gmail.com>
2024-04-05 15:27:30 -0700

    03 solved

--- pt-3/problems/03-least-and-most-expensive.js
+++ pt-3/problems/03-least-and-most-expensive.js
@@ -12,10 +12,43 @@ Least and Most Expensive Toys:
     console.log(leastAndMostExpensive(catalog2)); // prints ['doll', 'bicycle']
 */
 
+// my thinking is to compare to arrays to each other.
+//and push the key into the new array by matching it with
+//the index value of the corresponding number.
 function leastAndMostExpensive(catalog) {
-  // Your code here 
+let arrVal = Object.values(catalog)
+let arrKey = Object.keys(catalog)
+let numMax = 0
+let numMin = Infinity
+let ind1
+let ind2
+let arr = []
+
+for(let i = 0; i < arrVal.length; i++){
+    let el = arrVal[i]
+    if(numMax < el) numMax = el
+    ind1 = arrVal.indexOf(numMax)
+    if(numMin > el) numMin = el
+    ind2 = arrVal.indexOf(numMin)
 }
 
+for(let j = 0; j < arrKey.length; j++){
+    let el = arrKey[j]
+    if(arrKey.indexOf(el) === ind2) arr.push(el)
+}
+
+for(let k = 0; k < arrKey.length; k++){
+    let el = arrKey[k]
+    if(arrKey.indexOf(el) === ind1) arr.push(el)
+}
+return arr
+}
+
+
+    const catalog1 = {'ball': 5, 'jumprope': 3, 'yo-yo': 10, 'trading cards': 12}
+    const catalog2 = {'skateboard': 50, 'bicycle': 100, 'doll': 10, 'puzzle': 20}
+    console.log(leastAndMostExpensive(catalog1)); // prints ['jumprope', 'trading cards']
+    console.log(leastAndMostExpensive(catalog2)); // prints ['doll', 'bicycle']
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
