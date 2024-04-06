0cd2a1b895d2941606442e5ab0a2a246d43581c4
damonryon <damon.ryon@gmail.com>
2024-04-05 21:37:52 -0700

    06 solved

--- pt-3/problems/06-items-within-budget.js
+++ pt-3/problems/06-items-within-budget.js
@@ -13,16 +13,33 @@
                         {name: 'omakase bento box', price: 100},
                         {name: 'medicine', price: 50}
                     ]
-    
-    console.log(itemsWithinBudget(1000, items1)) // prints ['iPhone', 'guitar'] 
+
+    console.log(itemsWithinBudget(1000, items1)) // prints ['iPhone', 'guitar']
     console.log(itemsWithinBudget(200, items2)) // prints ['omakase bento box', 'medicine']
 */
 
 const itemsWithinBudget = (budget, items) => {
-    // Your code here 
+    let arr = []
+
+    for(let i = 0; i < items.length; i++){
+        let el = items[i]
+        if(el['price'] < budget) arr.push(el['name'])
+    }
+    return arr
 }
 
+const items1 = [{name: 'iPhone', price: 700},
+                      {name: 'laptop', price: 2000},
+                      {name: 'guitar', price: 300},
+                      {name: 'camera', price: 1200}
+                    ]
+    const items2 = [ {name: 'online classes', price: 1000},
+                        {name: 'omakase bento box', price: 100},
+                        {name: 'medicine', price: 50}
+                    ]
 
+    console.log(itemsWithinBudget(1000, items1)) // prints ['iPhone', 'guitar']
+    console.log(itemsWithinBudget(200, items2)) // prints ['omakase bento box', 'medicine']
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
