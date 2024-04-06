913ba9752ff4e2d45ec51472b48d5338bbff21eb
damonryon <damon.ryon@gmail.com>
2024-04-05 21:09:52 -0700

    05 solved

--- pt-3/problems/05-least-common-flavor.js
+++ pt-3/problems/05-least-common-flavor.js
@@ -15,9 +15,32 @@ Least common flavor:
 */
 
 function leastCommonFlavor(candyBag) {
-  // Your code here 
-}
+let keyArr = Object.keys(candyBag)
+let valArr = Object.values(candyBag)
+let minNum = Infinity
+let ind1
 
+
+for(let i = 0; i < valArr.length; i++){
+    let el = valArr[i]
+    if(el < minNum) minNum = el
+
+    ind1 = valArr.indexOf(minNum)
+
+}
+for(let j = 0; j < keyArr.length; j++){
+    let el = keyArr[j]
+    if(keyArr.indexOf(el) === ind1) return el
+}
+return null
+}
+const bag1 = { cherry: 4, grape: 5, lemon: 3, orange: 2, berry: 4 };
+    console.log(leastCommonFlavor(bag1)); // prints 'orange'
+
+    const bag2 = { cherry: 5, grape: 4, lemon: 2, orange: 3, chocolate: 6 }
+    console.log(leastCommonFlavor(bag2)); // prints 'lemon'
+
+    console.log(leastCommonFlavor({})); // prints null
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
