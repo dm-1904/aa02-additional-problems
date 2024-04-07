d1dad97933e952e0c3151b678b80fb8f6c124935
damonryon <damon.ryon@gmail.com>
2024-04-07 16:23:20 -0700

    01 solved

--- pt-4/problems/01-best-student.js
+++ pt-4/problems/01-best-student.js
@@ -1,5 +1,5 @@
 /*
-    
+
     Write a function bestStudent() that takes any number of student objects, each with names for keys, and total points for values.
     The function should return the name of the student with the most points.
     If no students are passed to the function, return null;
@@ -13,13 +13,30 @@
 
     console.log(bestStudent(student1, student2, student3, student4, student5)) // returns "Justin"
 
-    
+
 */
 
 const bestStudent = (...args) => {
-    // Your code here 
+    let num = 0
+    let arr = []
+    for(let obj of args){
+        arr.push(obj['points'])
+    }
+    for(let key of arr){
+        if(num < key) num = key
+    }
+    for(let el of args){
+        if(el['points'] === num) return el['name']
+    }
 }
 
+const student1 = { name: "Geoff", points: 50 };
+    const student2 = { name: "Justin", points: 1000 };
+    const student3 = { name: "Tom", points: 568 };
+    const student4 = { name: "Emily", points: 700 };
+    const student5 = { name: "Corina", points: 750 };
+
+    console.log(bestStudent(student1, student2, student3, student4, student5)) // returns "Justin"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
